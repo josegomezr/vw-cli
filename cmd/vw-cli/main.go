c1f250bbcc1a35cbe0a7e71bebcd44e293de4adf
Jose D. Gomez R. <1josegomezr@gmail.com>
2025-12-04 09:39:24 +0100

    Split VW struct from main and bring some arg parsing
    still very very raw, but sorta works pulling things out of the state

--- cmd/vw-cli/main.go
+++ cmd/vw-cli/main.go
@@ -1,249 +1,111 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"log"
-	"net/http"
-	"net/url"
+	"strings"
 	"os"
-	"time"
-
-	"github.com/josegomezr/vw-cli/internal/api"
+	"flag"
 	"github.com/josegomezr/vw-cli/internal/crypto"
-	"github.com/josegomezr/vw-cli/internal/symmetric_key"
 	"github.com/josegomezr/vw-cli/internal/utils"
 )
 
-// TODO: split this into separate files & packages
-type VWConfig struct {
-	BitwardenURL string `json:"bitwarden-url"`
-	ClientId     string `json:"client-id"`
-	ClientSecret string `json:"client-secret"`
-	Email        string `json:"email"`
-	Password     string `json:"password"`
+
+type CLIOpts struct {
+	ConfigDir string
+	Password string
+	MasterPassword string
 }
 
-type VWState struct {
-	AccessToken         string           `json:"access_token"`
-	ExpiresAtEpoch      int64            `json:"expires_at_epoch"`
-	ExpiresIn           int64            `json:"expires_in"`
-	Kdf                 int64            `json:"Kdf"`
-	KdfIterations       int64            `json:"KdfIterations"`
-	KdfMemory           string           `json:"KdfMemory"`
-	KdfParallelism      string           `json:"KdfParallelism"`
-	Key                 string           `json:"Key"`
-	PrivateKey          string           `json:"PrivateKey"`
-	ResetMasterPassword bool             `json:"ResetMasterPassword"`
-	LatestSync          api.SyncResponse `json:"latest_sync"`
-}
-
-type VW struct {
-	cfgdir        string
-	cfg           *VWConfig
-	state         *VWState
-	userKey       symmetric_key.SymmetricKey
-	asymmetricKey symmetric_key.SymmetricKey
-	allkeys       map[string]symmetric_key.SymmetricKey
-}
-
-// TODO: bring here a proper http client that takes care of the auth using the
-// transport interface.
-
-func (vw *VW) Login() (err error) {
-	if vw == nil {
-		return fmt.Errorf("not initialized")
-	}
-
-	if vw.cfg == nil {
-		return fmt.Errorf("not configured")
-	}
-
-	if vw.state != nil {
-		exp := time.Unix(vw.state.ExpiresAtEpoch, 0)
-
-		if time.Now().Before(exp) {
-			return nil
-		}
-	} else {
-		vw.state = &VWState{}
-	}
-
-	uri := "/identity/connect/token"
-	bwUrl := vw.cfg.BitwardenURL + uri
-
-	qs := url.Values{}
-	qs.Set("grant_type", "client_credentials")
-	qs.Set("scope", "api")
-	qs.Set("device_identifier", "vw-cli")
-	qs.Set("device_name", "vw-cli name")
-	qs.Set("device_type", "CLI")
-	qs.Set("client_id", vw.cfg.ClientId)
-	qs.Set("client_secret", vw.cfg.ClientSecret)
-
-	resp, err := http.PostForm(bwUrl, qs)
-
-	if err != nil {
-		return fmt.Errorf("http-error: %w", err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != 200 {
-		io.Copy(os.Stdout, resp.Body)
-		return fmt.Errorf("http-error-not-200")
-	}
-
-	if err := json.NewDecoder(resp.Body).Decode(vw.state); err != nil {
-		io.Copy(os.Stdout, resp.Body)
-		return fmt.Errorf("error-decode-state: %w", err)
-	}
-
-	// TODO: Extract this into SaveState()
-	file, err := os.Create(vw.cfgdir + "state.file")
-	if err != nil {
-		return fmt.Errorf("error-state-file: %w", err)
-	}
-	defer file.Close()
-	vw.state.ExpiresAtEpoch = time.Now().Add(time.Duration(vw.state.ExpiresIn) * time.Second).Unix()
-	if err := json.NewEncoder(file).Encode(vw.state); err != nil {
-		return fmt.Errorf("error-flush-state: %w", err)
-	}
-
-	syncObj, err := vw.Sync()
-	if err != nil {
-		return fmt.Errorf("sync-state: %w", err)
-	}
-
-	vw.state.LatestSync = *syncObj
-
-	file.Seek(0, 0)
-	if err := json.NewEncoder(file).Encode(vw.state); err != nil {
-		return fmt.Errorf("error-flush-state: %w", err)
-	}
-
+func argParse() *CLIOpts {
 	return nil
 }
 
-func (vw *VW) LoadConfig() error {
-	if vw == nil {
-		return fmt.Errorf("not initialized")
-	}
-
-	file, err := os.Open(vw.cfgdir + "config.config")
-	if err != nil {
-		return fmt.Errorf("error-open-config-file: %w", err)
-	}
-	defer file.Close()
-
-	cfg := VWConfig{}
-
-	if err := json.NewDecoder(file).Decode(&cfg); err != nil {
-		return fmt.Errorf("error-decode-config-file: %w", err)
-	}
-
-	vw.cfg = &cfg
-	return nil
-}
-
-func (vw *VW) LoadState() error {
-	state := &VWState{}
-	vw.state = state
-
-	file, err := os.Open(vw.cfgdir + "state.file")
-	if err != nil {
-		if err == io.EOF || os.IsNotExist(err) {
-			return nil
-		}
-		return fmt.Errorf("error-open: %w", err)
-	}
-	defer file.Close()
-
-	if err := json.NewDecoder(file).Decode(state); err != nil {
-		if err == io.EOF {
-			return nil
-		}
-		return fmt.Errorf("error-decode: %w", err)
-	}
-
-	return nil
-}
-
-func (vw *VW) DecryptUserKey() error {
-	masterkey, err := deriveDecryptionKeyFromEmailPassword(
-		vw.state.LatestSync.Profile.Email,
-		vw.cfg.Password,
-	)
-	if err != nil {
-		return err
-	}
-	{
-		encsymkey, err := crypto.NewEncStringFrom(vw.state.Key)
-		if err != nil {
-			return err
-		}
-
-		key, err := symmetric_key.AES_CBC_256_HMAC_decrypt(masterkey.Encryption(), masterkey.Authentication(), encsymkey.IV(), encsymkey.Data(), encsymkey.MAC())
-		if err != nil {
-			return err
-		}
-
-		symkey, err := symmetric_key.NewSymmetricKey(key)
-		if err != nil {
-			return err
-		}
-		vw.userKey = symkey
-	}
-	{
-		encsymkey, err := crypto.NewEncStringFrom(vw.state.PrivateKey)
-		if err != nil {
-			return err
-		}
-
-		key, err := symmetric_key.AES_CBC_256_HMAC_decrypt(vw.userKey.Encryption(), vw.userKey.Authentication(), encsymkey.IV(), encsymkey.Data(), encsymkey.MAC())
-		if err != nil {
-			return err
-		}
-
-		symkey, err := symmetric_key.NewSymmetricKey(key)
-		if err != nil {
-			return err
-		}
-		vw.asymmetricKey = symkey
-	}
-
-	return nil
-}
-
-func (vw *VW) DecryptOrganizationKeys() error {
-	if vw.allkeys == nil {
-		vw.allkeys = make(map[string]symmetric_key.SymmetricKey)
-	}
-	vw.allkeys[""] = vw.userKey
-
-	for _, orgObj := range vw.state.LatestSync.Profile.Organizations {
-		encsymkey, err := crypto.NewEncStringFrom(orgObj.Key)
-		if err != nil {
-			return err
-		}
-
-		key, err := symmetric_key.RSA_2048_OAEP_SHA_1_decrypt(vw.asymmetricKey.Encryption(), encsymkey.Data())
-		if err != nil {
-			return err
-		}
-
-		symkey, err := symmetric_key.NewSymmetricKey(key)
-		if err != nil {
-			return err
-		}
-
-		vw.allkeys[orgObj.Id] = symkey
-
-	}
-	return nil
+var flagvar int
+var GLOBAL_VW *VW
+func init() {
+	flag.IntVar(&flagvar, "n", 1234, "help message for flag n")
+	GLOBAL_VW = &VW{}
 }
 
 func main() {
+	cfgdir := "./state-files/"
+
+	GLOBAL_VW.cfgdir = cfgdir
+
+	if err := GLOBAL_VW.LoadConfig(); err != nil {
+		log.Fatalf("error-load-config: %s", err)
+	}
+
+	if err := GLOBAL_VW.LoadState(); err != nil {
+		log.Fatalf("error-load-state: %s", err)
+	}
+
+	flag.Parse()
+	cmd := flag.Arg(0)
+
+	switch cmd {
+	case "":
+		fallthrough
+	case "help":
+		flag.Usage()
+		return
+	case "show":
+		doShow(flag.Arg(1), flag.Arg(2))
+		return
+	default:
+		log.Printf("Unknown command: %s", cmd)
+		os.Exit(1)
+		return
+	}
+}
+
+func doShow(attr, id string){
+	// log.Printf("Showing %s of secret %s", attr, id)
+
+	if err := GLOBAL_VW.DecryptUserKey(); err != nil {
+		log.Fatalf("error-decrypt: %s", err)
+	}
+
+	if err := GLOBAL_VW.DecryptOrganizationKeys(); err != nil {
+		log.Fatalf("error-org-decrypt: %s", err)
+	}
+
+	for _, cipherObj := range GLOBAL_VW.state.LatestSync.Ciphers {
+		if id != cipherObj.Id {
+			continue
+		}
+		key, ok := GLOBAL_VW.allkeys[cipherObj.OrganizationId]
+		if !ok {
+			fmt.Printf("%s,%s,%s\n", cipherObj.Id, "TODO:no key known ", "~")
+			break
+		}
+
+		encryptedAttr := ""
+		switch attr {
+		case "password":
+			encryptedAttr = cipherObj.Login.Password
+		case "username":
+			encryptedAttr = cipherObj.Login.Username
+		default:
+			log.Fatalf("Unknown attribute %s", attr)
+			return
+		}
+
+		decryptedName, err := key.DecryptString(utils.Must2(crypto.NewEncStringFrom(encryptedAttr)))
+		if err != nil {
+			fmt.Printf("%s,%s,%s\n", cipherObj.Id, "TODO:cannot decrypt this yet", "~")
+			break
+		}
+		fmt.Printf(decryptedName)
+		return
+		_ = strings.Contains
+		// if strings.Contains(decryptedName, strings.ToLower(id))
+	}
+
+}
+
+func main2() {
 	cfgdir := "./state-files/"
 
 	vw := &VW{
@@ -304,42 +166,4 @@ func main() {
 			fmt.Printf("%s,%s,%s,%s\n", cipherObj.Id, folders[cipherObj.FolderId], cipher, pw)
 		}
 	}
-}
-
-// TODO: separate this into file/packages.
-
-func (vw *VW) ListFolders() ([]api.FolderObject, error) {
-	bwUrl := vw.cfg.BitwardenURL + "/api/folders"
-
-	req, err := http.NewRequest("GET", bwUrl, nil)
-	req.Header.Add("Authorization", "Bearer "+vw.state.AccessToken)
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("http-error: %w", err)
-	}
-	defer resp.Body.Close()
-
-	r := &api.FoldersResponse{}
-	if err := json.NewDecoder(resp.Body).Decode(r); err != nil {
-		return nil, fmt.Errorf("error-decode-state: %w", err)
-	}
-	return r.Data, nil
-}
-
-func (vw *VW) Sync() (*api.SyncResponse, error) {
-	bwUrl := vw.cfg.BitwardenURL + "/api/sync"
-
-	req, err := http.NewRequest("GET", bwUrl, nil)
-	req.Header.Add("Authorization", "Bearer "+vw.state.AccessToken)
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		io.Copy(os.Stdout, resp.Body)
-		return nil, fmt.Errorf("http-error: %w", err)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
-		io.Copy(os.Stdout, resp.Body)
-		return nil, fmt.Errorf("bad status code for sync")
-	}
-	return api.NewSyncResponseFromReader(resp.Body)
 }
---
+++ cmd/vw-cli/vw.go
@@ -0,0 +1,280 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"net/http"
+	"net/url"
+	"os"
+	"io"
+	"encoding/json"
+
+	"github.com/josegomezr/vw-cli/internal/api"
+	"github.com/josegomezr/vw-cli/internal/crypto"
+	"github.com/josegomezr/vw-cli/internal/symmetric_key"
+)
+
+// TODO: split this into separate files & packages
+type VWConfig struct {
+	BitwardenURL string `json:"bitwarden-url"`
+	ClientId     string `json:"client-id"`
+	ClientSecret string `json:"client-secret"`
+	Email        string `json:"email"`
+	Password     string `json:"password"`
+}
+
+type VWState struct {
+	AccessToken         string           `json:"access_token"`
+	ExpiresAtEpoch      int64            `json:"expires_at_epoch"`
+	ExpiresIn           int64            `json:"expires_in"`
+	Kdf                 int64            `json:"Kdf"`
+	KdfIterations       int64            `json:"KdfIterations"`
+	KdfMemory           string           `json:"KdfMemory"`
+	KdfParallelism      string           `json:"KdfParallelism"`
+	Key                 string           `json:"Key"`
+	PrivateKey          string           `json:"PrivateKey"`
+	ResetMasterPassword bool             `json:"ResetMasterPassword"`
+	LatestSync          api.SyncResponse `json:"latest_sync"`
+}
+
+type VW struct {
+	cfgdir        string
+	cfg           *VWConfig
+	state         *VWState
+	userKey       symmetric_key.SymmetricKey
+	asymmetricKey symmetric_key.SymmetricKey
+	allkeys       map[string]symmetric_key.SymmetricKey
+}
+
+// TODO: bring here a proper http client that takes care of the auth using the
+// transport interface.
+
+func (vw *VW) Login() (err error) {
+	if vw == nil {
+		return fmt.Errorf("not initialized")
+	}
+
+	if vw.cfg == nil {
+		return fmt.Errorf("not configured")
+	}
+
+	if vw.state != nil {
+		exp := time.Unix(vw.state.ExpiresAtEpoch, 0)
+
+		if time.Now().Before(exp) {
+			return nil
+		}
+	} else {
+		vw.state = &VWState{}
+	}
+
+	uri := "/identity/connect/token"
+	bwUrl := vw.cfg.BitwardenURL + uri
+
+	qs := url.Values{}
+	qs.Set("grant_type", "client_credentials")
+	qs.Set("scope", "api")
+	qs.Set("device_identifier", "vw-cli")
+	qs.Set("device_name", "vw-cli name")
+	qs.Set("device_type", "CLI")
+	qs.Set("client_id", vw.cfg.ClientId)
+	qs.Set("client_secret", vw.cfg.ClientSecret)
+
+	resp, err := http.PostForm(bwUrl, qs)
+
+	if err != nil {
+		return fmt.Errorf("http-error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		io.Copy(os.Stdout, resp.Body)
+		return fmt.Errorf("http-error-not-200")
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(vw.state); err != nil {
+		io.Copy(os.Stdout, resp.Body)
+		return fmt.Errorf("error-decode-state: %w", err)
+	}
+
+	// TODO: Extract this into SaveState()
+	file, err := os.Create(vw.cfgdir + "state.file")
+	if err != nil {
+		return fmt.Errorf("error-state-file: %w", err)
+	}
+	defer file.Close()
+	vw.state.ExpiresAtEpoch = time.Now().Add(time.Duration(vw.state.ExpiresIn) * time.Second).Unix()
+	if err := json.NewEncoder(file).Encode(vw.state); err != nil {
+		return fmt.Errorf("error-flush-state: %w", err)
+	}
+
+	syncObj, err := vw.Sync()
+	if err != nil {
+		return fmt.Errorf("sync-state: %w", err)
+	}
+
+	vw.state.LatestSync = *syncObj
+
+	file.Seek(0, 0)
+	if err := json.NewEncoder(file).Encode(vw.state); err != nil {
+		return fmt.Errorf("error-flush-state: %w", err)
+	}
+
+	return nil
+}
+
+func (vw *VW) LoadConfig() error {
+	if vw == nil {
+		return fmt.Errorf("not initialized")
+	}
+
+	file, err := os.Open(vw.cfgdir + "config.config")
+	if err != nil {
+		return fmt.Errorf("error-open-config-file: %w", err)
+	}
+	defer file.Close()
+
+	cfg := VWConfig{}
+
+	if err := json.NewDecoder(file).Decode(&cfg); err != nil {
+		return fmt.Errorf("error-decode-config-file: %w", err)
+	}
+
+	vw.cfg = &cfg
+	return nil
+}
+
+func (vw *VW) LoadState() error {
+	state := &VWState{}
+	vw.state = state
+
+	file, err := os.Open(vw.cfgdir + "state.file")
+	if err != nil {
+		if err == io.EOF || os.IsNotExist(err) {
+			return nil
+		}
+		return fmt.Errorf("error-open: %w", err)
+	}
+	defer file.Close()
+
+	if err := json.NewDecoder(file).Decode(state); err != nil {
+		if err == io.EOF {
+			return nil
+		}
+		return fmt.Errorf("error-decode: %w", err)
+	}
+
+	return nil
+}
+
+func (vw *VW) DecryptUserKey() error {
+	masterkey, err := deriveDecryptionKeyFromEmailPassword(
+		vw.state.LatestSync.Profile.Email,
+		vw.cfg.Password,
+	)
+	if err != nil {
+		return err
+	}
+	{
+		encsymkey, err := crypto.NewEncStringFrom(vw.state.Key)
+		if err != nil {
+			return err
+		}
+
+		key, err := symmetric_key.AES_CBC_256_HMAC_decrypt(masterkey.Encryption(), masterkey.Authentication(), encsymkey.IV(), encsymkey.Data(), encsymkey.MAC())
+		if err != nil {
+			return err
+		}
+
+		symkey, err := symmetric_key.NewSymmetricKey(key)
+		if err != nil {
+			return err
+		}
+		vw.userKey = symkey
+	}
+	{
+		encsymkey, err := crypto.NewEncStringFrom(vw.state.PrivateKey)
+		if err != nil {
+			return err
+		}
+
+		key, err := symmetric_key.AES_CBC_256_HMAC_decrypt(vw.userKey.Encryption(), vw.userKey.Authentication(), encsymkey.IV(), encsymkey.Data(), encsymkey.MAC())
+		if err != nil {
+			return err
+		}
+
+		symkey, err := symmetric_key.NewSymmetricKey(key)
+		if err != nil {
+			return err
+		}
+		vw.asymmetricKey = symkey
+	}
+
+	return nil
+}
+
+func (vw *VW) DecryptOrganizationKeys() error {
+	if vw.allkeys == nil {
+		vw.allkeys = make(map[string]symmetric_key.SymmetricKey)
+	}
+	vw.allkeys[""] = vw.userKey
+
+	for _, orgObj := range vw.state.LatestSync.Profile.Organizations {
+		encsymkey, err := crypto.NewEncStringFrom(orgObj.Key)
+		if err != nil {
+			return err
+		}
+
+		key, err := symmetric_key.RSA_2048_OAEP_SHA_1_decrypt(vw.asymmetricKey.Encryption(), encsymkey.Data())
+		if err != nil {
+			return err
+		}
+
+		symkey, err := symmetric_key.NewSymmetricKey(key)
+		if err != nil {
+			return err
+		}
+
+		vw.allkeys[orgObj.Id] = symkey
+
+	}
+	return nil
+}
+
+// TODO: separate this into file/packages.
+
+func (vw *VW) ListFolders() ([]api.FolderObject, error) {
+	bwUrl := vw.cfg.BitwardenURL + "/api/folders"
+
+	req, err := http.NewRequest("GET", bwUrl, nil)
+	req.Header.Add("Authorization", "Bearer "+vw.state.AccessToken)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("http-error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	r := &api.FoldersResponse{}
+	if err := json.NewDecoder(resp.Body).Decode(r); err != nil {
+		return nil, fmt.Errorf("error-decode-state: %w", err)
+	}
+	return r.Data, nil
+}
+
+func (vw *VW) Sync() (*api.SyncResponse, error) {
+	bwUrl := vw.cfg.BitwardenURL + "/api/sync"
+
+	req, err := http.NewRequest("GET", bwUrl, nil)
+	req.Header.Add("Authorization", "Bearer "+vw.state.AccessToken)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		io.Copy(os.Stdout, resp.Body)
+		return nil, fmt.Errorf("http-error: %w", err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != 200 {
+		io.Copy(os.Stdout, resp.Body)
+		return nil, fmt.Errorf("bad status code for sync")
+	}
+	return api.NewSyncResponseFromReader(resp.Body)
+}
